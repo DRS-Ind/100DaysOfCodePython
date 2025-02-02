90c485c01802a7a9da3031e5fea939980c7883c1
DRS-Ind <dimaradich@gmail.com>
2025-02-03 00:23:03 +0200

    Revive from vacation.

--- Day15(The Coffee Machine Project)/coffee_machine.py
+++ Day15(The Coffee Machine Project)/coffee_machine.py
@@ -13,12 +13,14 @@ class Coffee:
         return (f"Coffee {self.name} that contains {self.water} ml of water, {self.coffee} g of coffee and {self.milk} "
                 f"ml of milk. Price: ${self.price}.")
 
+
 class Espresso(Coffee):
     def __init__(self) -> None:
         """
         A cup of Coffee Espresso.
         """
         super().__init__(water=50, coffee=18, milk=0, price=1.5, name="Espresso")
+
 
 class Latte(Coffee):
     def __init__(self) -> None:
@@ -27,6 +29,7 @@ class Latte(Coffee):
         """
         super().__init__(water=200, coffee=24, milk=150, price=2.5, name="Latte")
 
+
 class Cappuccino(Coffee):
     def __init__(self) -> None:
         """
@@ -34,18 +37,19 @@ class Cappuccino(Coffee):
         """
         super().__init__(water=250, coffee=24, milk=100, price=3.0, name="Cappuccino")
 
+
 class CoffeeMachine:
     def __init__(self) -> None:
         """
         Coffee machine with already filled tanks.
         """
         self._water_tank = 300
-        self._milk_tank = 0
+        self._milk_tank = 200
         self._coffee_tank = 100
         self._money_bank = 0
         self._stand_by = True
         self._coffee_set = {"espresso": Espresso(), "latte": Latte(), "cappuccino": Cappuccino()}
-        self._possible_prompt = ["off"] + [coffee for coffee in self._coffee_set]
+        self._possible_prompt = ["off", "report"] + [coffee for coffee in self._coffee_set]
 
     def report(self) -> str:
         """
@@ -73,28 +77,61 @@ Money: ${self._money_bank}"""
             return False
         return True
 
+    def cashier(self, coffee: Coffee) -> None:
+        """
+        The internal function is to count cash and recalculate machine resources.
+        :param coffee: the coffee chosen by the user
+        :return:
+        """
+        print("Please insert coins.")
+        try:
+            quarters = int(input("How many quarters?: "))
+            dimes = int(input("How many dimes?: "))
+            nickles = int(input("How many nickles?: "))
+            pennies = int(input("How many pennies?: "))
+
+            # count money
+            inserted_value = pennies * .01 + nickles * .05 + dimes * .1 + quarters * .25
+            if inserted_value >= coffee.price:  # check if user`s money enough
+                self._water_tank -= coffee.water
+                self._milk_tank -= coffee.milk
+                self._coffee_tank -= coffee.coffee
+                self._money_bank += coffee.price
+                change = inserted_value - coffee.price
+
+                # print thank you messages
+                print(f"Here is ${round(change, 2)} in change")
+                print(f"Here is your {coffee.name} Enjoy!")
+            else:
+                print("Sorry that's not enough money. Money refunded.")
+        except ValueError:
+            print("You cannot insert this count of coins.")  # try again with money
+            self.cashier(coffee=coffee)
+
     def start_machine(self) -> None:
         """
         The main function for starting the coffee machine
         """
-        if self._stand_by:
+        # operation until shutdown and automatic transition to maintenance if there are not enough resources for
+        # the least demanding coffee
+        while self._stand_by and self.is_enough_resources(coffee=Espresso()):
             self.check_user_prompt()
-        else: print("Machine under maintenance.")
+        print("Machine under maintenance.")
 
     def check_user_prompt(self) -> None:
         """
         The function checks users` prompts.
         """
-        user_prompt = input(f"What would you like? ({'/'.join(self._possible_prompt[1:])})").lower()
-        if user_prompt == "off":
-            print("Off")
+        user_prompt = input(f"What would you like? ({'/'.join(self._possible_prompt[2:])}): ").lower()
+        if user_prompt == "off":  # for shutting down
             self._stand_by = False
-        else:
-            if user_prompt in self._possible_prompt[1:]:
-                if self.is_enough_resources(coffee=self._coffee_set[user_prompt]):
-                    print("Coffee")
-            else: print(f"Wrong coffee name. Possible coffee: {", ".join(self._possible_prompt[1:])}")
-            self.check_user_prompt()
+        elif user_prompt == "report":  # getting report
+            print(self.report())
+        elif user_prompt in self._possible_prompt[1:]:  # get a coffee
+            if self.is_enough_resources(coffee=self._coffee_set[user_prompt]):
+                self.cashier(self._coffee_set[user_prompt])
+        else:  # try again
+            print(f"Wrong coffee name. Possible coffee: {", ".join(self._possible_prompt[2:])}")
 
 
 if __name__ == '__main__':

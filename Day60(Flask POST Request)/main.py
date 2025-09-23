87516d645b351d60dfdc14622f6c38c555e7f6d8
DRS-Ind <dimaradich@gmail.com>
2025-09-23 23:00:08 +0300

    60th day of coding, add the documentation to the project.

--- Day60(Flask POST Request)/main.py
+++ Day60(Flask POST Request)/main.py
@@ -17,24 +17,39 @@ curr_year = datetime.datetime.now().year
 
 @app.route("/")
 def home():
+    """
+    The home page.
+    """
     return render_template(template_name_or_list="index.html", posts=post_data, current_year=curr_year)
 
 
 @app.route("/contact", methods=["GET"])
 def contact():
+    """
+    The contact page.
+    """
     return render_template(template_name_or_list="contact.html", current_year=curr_year)
 
 
 @app.route("/about")
 def about():
+    """
+    The about page.
+    """
     return render_template(template_name_or_list="about.html", current_year=curr_year)
 
 
 @app.route("/post/<int:post_id>")
 def post(post_id: int):
+    """
+    The single post page.
+    :param post_id: the post id number
+    """
+    # catch the wrong post
     try:
         post_info = post_data[post_id - 1]
     except IndexError:
+        # redirect to the oops page
         return render_template(template_name_or_list="response.html", current_year=curr_year, title="Error 404",
                                subtitle="Your page is not found.")
     else:
@@ -43,6 +58,9 @@ def post(post_id: int):
 
 @app.route("/contact", methods=["POST"])
 def receive_form_data():
+    """
+    The POST method catch form values and send the message with them.
+    """
     form_name = request.form['name']
     form_email = request.form['email']
     form_phone = request.form['tel']
@@ -52,12 +70,14 @@ def receive_form_data():
     completed_message = (f"Get response from {form_name}.\n"
                          f"Email: {form_email}. Phone: {form_phone}.\n"
                          f"Message: {form_message}")
+    # catch error with connection to the SMTP server
     try:
         with smtplib.SMTP_SSL("smtp.gmail.com", port=465, context=ssl.create_default_context()) as connection:
             connection.login(user=company_email, password=company_email_password)
             connection.sendmail(from_addr=company_email, to_addrs=company_email,
                                 msg=f"Subject:Response from form\n\n{completed_message}")
     except (smtplib.SMTPException, socket.gaierror):
+        # redirect to the oops page
         return render_template(template_name_or_list="response.html", current_year=curr_year, title="Error 500",
                                subtitle="Your message has not been sent.")
     return render_template(template_name_or_list="response.html", current_year=curr_year, title="Hooray",
